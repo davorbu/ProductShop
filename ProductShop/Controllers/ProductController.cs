38a02e59622f1a0a1338b3be552ef67558394c0c
Davor Burmudžija <davorburmudzija@gmail.com>
2022-03-17 20:43:08 +0100

    Add project files.

---
+++ ProductShop/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductShop.Models;
+
+namespace ProductShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Detail(string slugLink)
+        {
+            Product product = DataBase.GetProduct(slugLink);
+            return View(product);
+        }
+
+        public IActionResult List()
+        {
+            List<Product> products = DataBase.GetProducts();
+            return View(products);
+        }
+    }
+}
